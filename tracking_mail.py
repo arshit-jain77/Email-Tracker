a7a2f1c8b673985c8717f12b434e425bbe2e7e43
Arshit <64263871+arshit-jain77@users.noreply.github.com>
2020-08-14 11:38:46 +0530

    Python Pytracking Implementation

---
+++ tracking_mail.py
@@ -0,0 +1,45 @@
+import smtplib
+import pytracking
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+fromaddr = "tigersem4D@gmail.com"
+toaddr = "arshitjain6@gmail.com"
+msg = MIMEMultipart()
+
+#Not sure if this is the right location for the pixel
+# url = pytracking.get_click_tracking_url()
+# full_url = "https://trackingdomain.com/path/e30203jhd9239754jh21387293jhf989sda="
+
+# print("pixel", pixel)
+
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "grande"
+body = "bla bla bla"
+msg.attach(MIMEText(body, 'plain'))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login('tigersem4D@gmail.com','tiger@4D')
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+
+print('DONE')
+
+
+open_tracking_url = pytracking.get_open_tracking_url(
+    {"customer_id": 1}, base_open_tracking_url="https://trackingdomain.com/path/",
+    webhook_url="http://requestb.in/123", include_webhook_url=True)
+
+print("open_tracking_url : ",open_tracking_url)
+
+tracking_result = pytracking.get_open_tracking_result(open_tracking_url, base_open_tracking_url="https://trackingdomain.com/path/")
+print("tracking_result : ",tracking_result)
+
+from pytracking.webhook import send_webhook
+send_webhook(tracking_result)
+
+pytracking.TrackingResult
+
