c1aad7e31081595306f7cd8bdc4d8fe145c620aa
Arshit <64263871+arshit-jain77@users.noreply.github.com>
2020-08-15 12:08:24 +0530

    Update email_track.php

--- email_tracker_in_python/email_track.php
+++ email_tracker_in_python/email_track.php
@@ -1,22 +1,5 @@
 <?php
 
-//email_track.php
-
-// $connect = new PDO("mysql:host=localhost;dbname=talenq", "root", "");
-
-// if(isset($_GET["code"]))
-// {
-// 	echo "inside email_track";
-// 	$query = "
-// 	UPDATE email_data 
-// 	SET email_status = 'yes', email_open_datetime = '".date("Y-m-d H:i:s", STRTOTIME(date('h:i:sa')))."' 
-// 	WHERE email_track_code = '".$_GET["code"]."' 
-// 	AND email_status = 'no'
-// 	";
-// 	$statement = $connect->prepare($query);
-// 	$statement->execute();
-// }
-
 $connect = new PDO("mysql:host=localhost;dbname=talenq", "root", "");
 
 if(isset($_GET["code"]))
